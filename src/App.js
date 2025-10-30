bdbafaa19f5b0b7f67b4d65692ddd31c3dcbc77d
Nikhilll008 <npatil96726@gmail.com>
2025-10-30 16:42:42 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import './login.css';
 
-function App() {
+const Login = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="login-container">
+      <div className="logo">
+        <svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 640 640"
+  width="40"
+  height="40"
+      svg>
+
+    
+          <path
+            fill="#4caf50"
+            d="M576 96C576 204.1 499.4 294.3 397.6 315.4C389.7 257.3 363.6 205 325.1 164.5C365.2 104 433.9 64 512 64L544 64C561.7 64 576 78.3 576 96zM64 160C64 142.3 78.3 128 96 128L128 128C251.7 128 352 228.3 352 352L352 544C352 561.7 337.7 576 320 576C302.3 576 288 561.7 288 544L288 384C164.3 384 64 283.7 64 160z"
+          />
+        </svg>
+        <h1>GreenEye</h1>
+      </div>
+
+      <h2>Welcome</h2>
+      <p>Login to your account</p>
+
+      <form>
+        <div className="input-group">
+          <input type="email" id="email" name="email" required />
+          <label htmlFor="email">Email Address</label>
+        </div>
+
+        <div className="input-group">
+          <input type="password" id="password" name="password" required />
+          <label htmlFor="password">Password</label>
+        </div>
+
+        <a href="#" className="forgot-password">
+          Forgot Password?
         </a>
-      </header>
+
+        <button type="submit" className="login-button">
+          Secure Login
+        </button>
+      </form>
+
+      <div className="signup-link">
+        <p>
+          Don’t have an account? <a href="#">Sign Up</a>
+        </p>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Login;
+
